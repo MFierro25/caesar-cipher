7d7dd98af4c644827a70e24f0bd13b7ff0d7c44a
Miguel Fierro <emfierro2@gmail.com>
2021-12-22 13:16:08 -0800

    working encrypt and decrypt

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -3,15 +3,16 @@ def encrypt(text, key):
     
     for char in text:
         # checks to see if string is empty ( no change happens)
-        if char == '':
-            cipher = cipher + char
         # checks upper case
-        elif char.isupper():
-            cipher = cipher + chr((ord(char)+key-65) % 26 + 65)
+        if char.isupper():
+            cipher += chr((ord(char)+key-65) % 26 + 65)
         # checks lower case
+        elif char.islower():
+            cipher += chr((ord(char)+key-97) % 26 + 97)
+        # checks for characters NOT letters
         else:
-            cipher = cipher + chr((ord(char)+key-97) % 26 + 97)
-
+            cipher += char
+            
     return cipher
 
 def decrypt(encoded, key):
--- tests/test_caesar_cipher.py
+++ tests/test_caesar_cipher.py
@@ -1,4 +1,4 @@
-from caesar_cipher.caesar_cipher import encrypt
+from caesar_cipher.caesar_cipher import decrypt, encrypt
 
 def test_encrypt_shift_1():
     actual = encrypt('car', 1)
@@ -18,4 +18,18 @@ def test_encrypt_upper():
 def test_encrypt_space():
     actual = encrypt('my one car', 1)
     expected = 'nz pof dbs'
+    assert actual == expected
+    
+def test_non_alpha():
+    actual = encrypt("Gimme 1 car!", 1)
+    expected = "Hjnnf 1 dbs!"
+    assert actual == expected
+    
+def test_decrypt():
+    phrase = 'I Made this work!'
+    shift = 2
+    encrypted = encrypt(phrase, shift)
+    
+    actual = decrypt(encrypted, shift)
+    expected = phrase
     assert actual == expected
